ff4f7921bdec9db27021fd6c9dcb313a5db44692
erick4556 <erickcedeno46@hotmail.com>
2021-08-26 19:17:26 -0500

    Cleaning code from product details

--- src/containers/ProductDetails.js
+++ src/containers/ProductDetails.js
@@ -1,5 +1,4 @@
 import React, { useEffect } from "react";
-import axios from "axios";
 import { useParams } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import {
